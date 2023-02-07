cdc89411f9c10cd035a9d8949216e265168c4399
Raghida Nweiran <116009953+RaghidaN@users.noreply.github.com>
2023-02-07 08:51:35 -0500

    Made Delete button bigger, says Delete now

--- src/Deck/DeckList.js
+++ src/Deck/DeckList.js
@@ -53,11 +53,11 @@ export default function DeckList({ decks }) {
               </div>
               <div className="">
                 <button
-                  className="btn btn-danger"
+                  className="btn btn-danger mr-2"
                   type="button"
                   onClick={() => handleDelete(deck.id)}
                 >
-                  <i className="fa-solid fa-trash-can"></i>
+                  <i className="fa-solid fa-trash-can"></i> Delete
                 </button>
               </div>
             </div>
